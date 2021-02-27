839a6dfed98d871b689ea04ae66c454c922d7189
John Greibe Larsen <johnglarsen@hotmail.co.uk>
2021-02-27 15:19:34 +0000

    Identified diagonal constraints for a diagonal Sudoku puzzles

--- solution.py
+++ solution.py
@@ -8,8 +8,8 @@ square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','45
 unitlist = row_units + column_units + square_units
 
 # TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
-
+diagonal_units = [[box[0]+''+box[1] for box in zip(rows, cols)], [box[0]+''+box[1] for box in zip(rows[::-1], cols)]]
+unitlist += diagonal_units
 
 # Must be called after all units (including diagonals) are added to the unitlist
 units = extract_units(unitlist, boxes)
